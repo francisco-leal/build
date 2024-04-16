4bcf411e3cad53b3e7064860ec251544ecac3351
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-04-16 12:43:44 +0100

    draft routes

---
+++ src/app/api/nominate.ts
@@ -0,0 +1,6 @@
+import { type NextRequest } from 'next/server'
+
+export async function POST(request: NextRequest) {
+    const res = await request.json()
+    return Response.json({ res })
+}
---
+++ src/app/api/search.ts
@@ -0,0 +1,7 @@
+import { type NextRequest } from 'next/server'
+
+export function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+    // query is "hello" for /api/search?query=hello
+}
