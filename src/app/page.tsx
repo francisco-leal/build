c381fd3dad0663ece0ec7169762a7c05ab144fc3
Tiago Gil <150550702+thetiagogil@users.noreply.github.com>
2024-04-25 18:29:07 +0100

    Finish home page layout (#10)
    * finish home page layout
    
    * change navbar order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
 
                     <Box sx={{ display: 'flex', gap: '24px' }}>
                         <Link sx={{ color: colors.white }}>Memo</Link>
-                        <Link sx={{ color: colors.white }}>Airdrop</Link>
                         <Link sx={{ color: colors.white }}>Bossenomics</Link>
+                        <Link sx={{ color: colors.white }}>Airdrop</Link>
                     </Box>
 
                     <Button
@@ -46,65 +46,84 @@ export default function Home() {
             {/* SECTION 1 */}
             <Box sx={{ padding: '144px 0 80px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Box
-                    sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '600px', gap: '40px' }}
+                    sx={{
+                        width: '1224px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
                 >
                     <Box
                         sx={{
                             display: 'flex',
                             flexDirection: 'column',
                             alignItems: 'center',
-                            gap: '16px'
+                            width: '600px',
+                            gap: '40px'
                         }}
                     >
-                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <Typography
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                gap: '16px'
+                            }}
+                        >
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Typography
+                                    sx={{
+                                        color: colors.white,
+                                        fontSize: '64px',
+                                        fontWeight: 'bold',
+                                        textAlign: 'center',
+                                        lineHeight: '1'
+                                    }}
+                                >
+                                    Dear builder,
+                                    <br /> you're fired.
+                                </Typography>
+                            </Box>
+
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Typography
+                                    sx={{
+                                        color: colors.white,
+                                        fontSize: '18px',
+                                        fontWeight: 'bold',
+                                        textAlign: 'center'
+                                    }}
+                                >
+                                    There's no room for builders in the corporate world. Read the memo and help us keep
+                                    the status quo: nominate the builders you know.
+                                </Typography>
+                            </Box>
+                        </Box>
+
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+                            <Button
+                                variant="outlined"
+                                size="md"
                                 sx={{
                                     color: colors.white,
-                                    fontSize: '64px',
-                                    fontWeight: 'bold',
-                                    textAlign: 'center',
-                                    lineHeight: '1'
+                                    borderRadius: '0%'
                                 }}
                             >
-                                Dear builder, you're fired.
-                            </Typography>
-                        </Box>
+                                Read BOSS Memo
+                            </Button>
 
-                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <Typography
-                                sx={{ color: colors.white, fontSize: '18px', fontWeight: 'bold', textAlign: 'center' }}
+                            <Button
+                                variant="solid"
+                                size="md"
+                                sx={{
+                                    color: colors.blue,
+                                    backgroundColor: colors.white,
+                                    borderRadius: '0%'
+                                }}
                             >
-                                There's no room for builders in the corporate world. Read the memo and help us keep the
-                                status quo: nominate the builders you know.
-                            </Typography>
+                                Mint Builders Manifesto
+                            </Button>
                         </Box>
-                    </Box>
-
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-                        <Button
-                            variant="outlined"
-                            onClick={() => open()}
-                            size="md"
-                            sx={{
-                                color: colors.white,
-                                borderRadius: '0%'
-                            }}
-                        >
-                            Read BOSS Memo
-                        </Button>
-
-                        <Button
-                            variant="solid"
-                            onClick={() => open()}
-                            size="md"
-                            sx={{
-                                color: colors.blue,
-                                backgroundColor: colors.white,
-                                borderRadius: '0%'
-                            }}
-                        >
-                            Mint Builders Manifesto
-                        </Button>
                     </Box>
                 </Box>
             </Box>
@@ -125,7 +144,7 @@ export default function Home() {
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        gap: '26px'
+                        gap: '40px'
                     }}
                 >
                     <Box
@@ -297,6 +316,198 @@ export default function Home() {
                             </tbody>
                         </Table>
                     </Box>
+
+                    <Typography sx={{ color: colors.white, fontSize: '14px' }}>
+                        Last update on May 21st, 4pm UTC. Next update on May 30, 4pm UTC
+                    </Typography>
+                </Box>
+            </Box>
+
+            {/* SECTION 4 */}
+            <Box
+                sx={{
+                    padding: '80px 0 80px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <Box
+                    sx={{
+                        width: '1224px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '40px'
+                    }}
+                >
+                    <Typography sx={{ color: colors.white, fontSize: '40px', fontWeight: 'bold' }}>
+                        Become a BOSS
+                    </Typography>
+
+                    <Box sx={{ display: 'flex', gap: '24px' }}>
+                        <Box
+                            sx={{
+                                height: '100%',
+                                width: '50%'
+                            }}
+                        >
+                            <Box
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                            >
+                                <Box
+                                    sx={{
+                                        height: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        padding: '40px',
+                                        gap: '40px'
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            gap: '8px'
+                                        }}
+                                    >
+                                        <Typography sx={{ color: colors.white, fontSize: '24px', fontWeight: 'bold' }}>
+                                            Step 1
+                                        </Typography>
+
+                                        <Typography
+                                            sx={{
+                                                color: colors.white,
+                                                fontSize: '18px',
+                                                textAlign: 'center',
+                                                lineHeight: '1'
+                                            }}
+                                        >
+                                            Claim your Talent Passport and increase your Builder Score before the first
+                                            snapshot, on May 7th at 17:59 UTC. The bigger your Builder Score, the higher
+                                            your daily budget.
+                                        </Typography>
+                                    </Box>
+
+                                    <Button
+                                        variant="solid"
+                                        size="md"
+                                        sx={{
+                                            color: colors.blue,
+                                            backgroundColor: colors.white,
+                                            borderRadius: '0%'
+                                        }}
+                                    >
+                                        Claim Talent Passport
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Box>
+
+                        <Box
+                            sx={{
+                                height: '100%',
+                                width: '50%'
+                            }}
+                        >
+                            <Box
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                            >
+                                <Box
+                                    sx={{
+                                        height: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        padding: '40px',
+                                        gap: '40px'
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            gap: '8px'
+                                        }}
+                                    >
+                                        <Typography sx={{ color: colors.white, fontSize: '24px', fontWeight: 'bold' }}>
+                                            Step 2
+                                        </Typography>
+
+                                        <Typography
+                                            sx={{
+                                                color: colors.white,
+                                                fontSize: '18px',
+                                                textAlign: 'center',
+                                                lineHeight: '1'
+                                            }}
+                                        >
+                                            Join us on party.app until May 7th, to help us raise LP funds and launch the
+                                            $BOSS token together on May 8th! Read more in the Bossenomics.
+                                        </Typography>
+                                    </Box>
+
+                                    <Button
+                                        variant="solid"
+                                        size="md"
+                                        sx={{
+                                            color: colors.blue,
+                                            backgroundColor: colors.white,
+                                            borderRadius: '0%'
+                                        }}
+                                    >
+                                        Join Party
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+
+            {/* FOOTER */}
+            <Box
+                sx={{
+                    padding: '48px 0 32px 0',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <Box
+                    sx={{
+                        width: '1224px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '24px'
+                    }}
+                >
+                    <Typography sx={{ color: colors.white, fontSize: '51px' }}>BOSS</Typography>
+
+                    <Box sx={{ display: 'flex', gap: '24px' }}>
+                        <Link sx={{ color: colors.white }}>Buy</Link>
+                        <Link sx={{ color: colors.white }}>Farcaster</Link>
+                        <Link sx={{ color: colors.white }}>Telegram</Link>
+                        <Link sx={{ color: colors.white }}>Party.app</Link>
+                        <Link sx={{ color: colors.white }}>FAQ</Link>
+                        <Link sx={{ color: colors.white }}>Meme templates</Link>
+                    </Box>
+
+                    <Typography sx={{ color: colors.white }}>
+                        BOSS is an experimental community project, not directly owned by{' '}
+                        <Link sx={{ color: colors.white, textDecoration: 'underline' }}>Talent Protocol</Link>. Not
+                        Financial Advice. DYOR.
+                    </Typography>
                 </Box>
             </Box>
         </main>
