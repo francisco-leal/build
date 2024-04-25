e7729041fc3a9523053bb00f0e153721ecca268b
thetiagogil <talmeidagil@hotmail.com>
2024-04-25 22:17:47 +0100

    finish memo page

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 'use client';
-import { Card, Typography } from '@mui/joy';
-// import { useTheme } from '@mui/joy/styles';
+import { Box, Typography } from '@mui/joy';
+import { NavBar } from '../shared/components/nav-bar';
+import { Footer } from '../shared/components/footer';
 
 const content = [
     'Dear builder,',
@@ -16,48 +17,55 @@ const content = [
 ];
 
 export default function Memo() {
-    // const theme = useTheme();
+    const colors = { blue: '#0142F5', white: '#FBFCFE', lightBlue: '#CDD7E1', black: '#0B0D0E', grey: '#636B74' };
     return (
-        <main
-            style={{
-                backgroundColor: 'blue',
-                margin: 0,
-                height: '100%',
-                width: '100%',
+        <main>
+            <NavBar />
+
+            <Box sx={{
+                padding: '80px 0 80px',
                 display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center'
-            }}
-        >
-            <Card
-                variant="plain"
-                style={{
-                    backgroundColor: 'white',
-                    padding: '120px 208px',
-                    maxWidth: '300px',
-                    margin: '18px 0px',
-                    // [theme.breakpoints.up('xs')]: {
-                    //     maxWidth: '300px',
-                    //     margin: '18px 0px',
-                    //   },
-                    //   [theme.breakpoints.up('sm')]: {
-                    //     maxWidth: '650px',
-                    //     margin: '80px 0px',
-                    //   },
-                    borderRadius: 0,
-                    borderWidth: '4px',
-                    borderColor: 'black',
-                    borderStyle: 'solid',
-                    boxShadow: '10px 10px 0px 0px black'
-                }}
-            >
-                <Typography level="h1">Dear builder, you’re fired.</Typography>
-                {content.map((line, index) => (
-                    <Typography key={index} level="body-md" style={{ padding: '20px 0px' }}>
-                        {line}
-                    </Typography>
-                ))}
-            </Card>
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Box
+                    sx={{
+                        backgroundColor: colors.white,
+                        width: "1224px",
+                        boxShadow: `12px 12px 0px 0px ${colors.black}`,
+                        border: `4px solid ${colors.black}`
+                        // [theme.breakpoints.up('xs')]: {
+                        //     maxWidth: '300px',
+                        //     margin: '18px 0px',
+                        //   },
+                        //   [theme.breakpoints.up('sm')]: {
+                        //     maxWidth: '650px',
+                        //     margin: '80px 0px',
+                        //   },
+                    }}
+                >
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: "column",
+                        gap: "32px",
+                        padding: "120px 208px 120px 208px",
+                    }}>
+                        <Typography sx={{fontSize: "56px", fontWeight: "bold"}}>Dear builder, you're fired.</Typography>
+
+                        <Box sx={{display: 'flex',
+                        flexDirection: "column",
+                        gap: "32px",}}>
+                            {content.map((line, index) => (
+                                <Typography key={index} sx={{fontSize: "18px", fontWeight: "600"}}>
+                                    {line}
+                                </Typography>
+                            ))}
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Footer />
         </main>
     );
 }
