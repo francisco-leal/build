ab851750199608483b3f8da466fcd56ee1549c99
Francisco Leal <francisco@talentprotocol.com>
2024-04-25 12:19:19 +0100

    Setup frontend dependencies (#1)

---
+++ src/app/config/index.tsx
@@ -0,0 +1,28 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+
+import { cookieStorage, createStorage } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+
+// Get projectId at https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+if (!projectId) throw new Error('Project ID is not defined');
+
+const metadata = {
+    name: '$BOSS',
+    description: '$boss',
+    url: 'https://talentprotocol.com', // origin must match your domain & subdomain
+    icons: ['https://talentprotocol.com/images/favicon.jpg']
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage
+    })
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+import { Typography, Button } from '@mui/joy';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+
 export default function Home() {
-    return <p>Hello</p>;
+    const { open } = useWeb3Modal();
+
+    return (
+        <main>
+            <Typography level="h1">Hello world</Typography>
+            <Button variant="solid" onClick={() => open()}>
+                Open
+            </Button>
+        </main>
+    );
 }
