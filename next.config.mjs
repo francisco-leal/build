62273f376b587c5658a7392f5c84b5a604f70e8f
PupoSDC <pupo163@hotmail.com>
2024-04-29 09:08:12 +0100

    feat: remove unrelated change

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        typedRoutes: true,
-    },
     i18n: {
         locales: ['en-US', 'zh-CN'],
         defaultLocale: 'en-US'
