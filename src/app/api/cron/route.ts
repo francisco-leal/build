666d2e085e31ef62b747fbb9f82dd87f00b50eeb
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-04-30 12:28:20 +0100

    add auth header to cron endpoint

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,10 +1,16 @@
+import { NextRequest } from "next/server";
 import {
   computeLeaderboard,
   computeUserNominationsAndStats,
   getNominationsFromFarcaster,
 } from "@/services";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   // cron job endpoint
   await getNominationsFromFarcaster();
   await computeUserNominationsAndStats();
