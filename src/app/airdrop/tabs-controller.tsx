d3952647bc54627108a12800479ecfac69da291a
thetiagogil <talmeidagil@hotmail.com>
2024-04-30 09:09:19 +0100

    finish airdroppage v2

--- src/app/airdrop/tabs-controller.tsx
+++ src/app/airdrop/tabs-controller.tsx
@@ -4,23 +4,44 @@ import { Section2 } from './section-2';
 import { Section3 } from './section-3';
 import { Section4 } from './section-4';
 import { Section5 } from './section-5';
+import { Section6 } from './section-6';
 
 export const TabsController = () => {
     return (
-        <Tabs aria-label="Basic tabs" defaultValue={0} sx={{ pt: 1, backgroundColor: "transparent" }}>
-            <TabList sx={{ justifyContent: "center", borderBottom: '1px solid rgba(255, 255, 255, 0.3)' }}>
-                <Tab sx={{ color: 'common.white' }}>My Nominations</Tab>
-                <Tab sx={{ color: 'common.white' }}>My Stats</Tab>
+        <Tabs aria-label="Basic tabs" defaultValue={0} sx={{ pt: 1, backgroundColor: 'transparent' }}>
+            <TabList sx={{ justifyContent: 'center', borderBottom: '1px solid rgba(255, 255, 255, 0.3)' }}>
+                <Tab
+                    variant="plain"
+                    sx={{
+                        color: 'common.white',
+                        '&.Mui-selected': { backgroundColor: 'transparent' },
+                        '&:hover': { backgroundColor: 'transparent' }
+                    }}
+                >
+                    My Nominations
+                </Tab>
+                <Tab
+                    variant="plain"
+                    sx={{
+                        color: 'common.white',
+                        '&.Mui-selected': { backgroundColor: 'transparent' },
+                        '&:hover': { backgroundColor: 'transparent' }
+                    }}
+                >
+                    My Stats
+                </Tab>
             </TabList>
             <TabPanel value={0}>
-                {/* <Section1 />
+                <Section1 />
                 <Section2 />
                 <Section3 />
-                <Section4 /> */}
-                <Section5 />
+                <Section4 />
             </TabPanel>
             <TabPanel value={1}>
+                <Section5 />
+                <Section6 />
+                <Section4 />
             </TabPanel>
         </Tabs>
     );
-}
+};
