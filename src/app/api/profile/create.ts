934d0b34f697d079b3567d8061b4449167f83525
francisco-leal <francisco@talentprotocol.com>
2024-05-07 11:15:26 +0100

    Check past nominations before creating user

--- src/app/api/profile/create.ts
+++ src/app/api/profile/create.ts
@@ -24,7 +24,7 @@ export async function createProfile(wallet_address: string) {
   // calculate boss_budget
   // we ignore the boss points and nomination streak at this point, given both are zero!
   const fid = socialProfiles.filter(
-    (profile) => profile.dapp === "farcaster",
+    (profile) => profile.dapp === "farcaster"
   )?.[0]?.profileTokenId;
 
   const boss_budget =
@@ -32,19 +32,43 @@ export async function createProfile(wallet_address: string) {
       ? fid > 20_000
         ? 500
         : 1000
-      : (builder_score * 20 + boss_tokens * 0.01) *
+      : (builder_score * 20 + boss_tokens * 0.001) *
         (has_manifesto_nft ? 1.2 : 1);
 
   const randomString = Buffer.from(crypto.randomUUID()).toString("base64");
   const inviteCode = randomString.slice(0, 8);
 
-  // @TODO: check if user did any nominations before being created to add the correct boss_score
+  const { data: past_given_nominations } = await supabase
+    .from("boss_nominations")
+    .select("*")
+    .eq("wallet_origin", wallet_address);
+
+  let boss_score = 0;
+  boss_score +=
+    past_given_nominations && past_given_nominations.length > 0
+      ? past_given_nominations.reduce((acc, nomination) => {
+          return acc + nomination.boss_points_earned;
+        }, 0)
+      : 0;
+
+  const { data: past_earned_nominations } = await supabase
+    .from("boss_nominations")
+    .select("*")
+    .eq("wallet_destination", wallet_address);
+
+  boss_score +=
+    past_earned_nominations && past_earned_nominations.length > 0
+      ? past_earned_nominations?.reduce((acc, nomination) => {
+          return acc + nomination.boss_points_earned;
+        }, 0)
+      : 0;
+
   const { data, error } = await supabase.from("users").insert({
     wallet: wallet_address,
     referral_code: inviteCode,
     username: username,
     manifesto_nft: has_manifesto_nft,
-    boss_score: 0,
+    boss_score,
     boss_budget,
     passport_builder_score: builder_score,
     boss_token_balance: boss_tokens,
