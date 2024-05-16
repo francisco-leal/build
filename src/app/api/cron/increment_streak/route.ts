a29213269d800eb31f42dcc40148e82b8b9b963a
francisco-leal <francisco@talentprotocol.com>
2024-05-16 17:13:35 +0100

    Fix: nomination streak calculation

---
+++ src/app/api/cron/increment_streak/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest } from "next/server";
+import { DateTime } from "luxon";
+import { JobTypes } from "@/app/_api/helpers/job-types";
+import { supabase } from "@/db";
+import { incrementStreaks } from "@/services/crons";
+
+export const maxDuration = 300;
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { data: resetMissedStreaksUpdate } = await supabase
+    .from("scheduled_updates")
+    .insert({
+      job_type: "nomination_streak" as JobTypes,
+      started_at: DateTime.utc().toISODate(),
+    })
+    .select("*")
+    .single();
+
+  await incrementStreaks();
+
+  if (resetMissedStreaksUpdate) {
+    await supabase
+      .from("scheduled_updates")
+      .update({
+        finished_at: DateTime.utc().toISODate(),
+      })
+      .eq("id", resetMissedStreaksUpdate.id);
+  }
+
+  return Response.json({}, { status: 200 });
+}
