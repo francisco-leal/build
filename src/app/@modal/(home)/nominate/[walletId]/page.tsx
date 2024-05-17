a174c8edc75d414f3c1bdf12f4cd6e0d5a213733
francisco-leal <francisco@talentprotocol.com>
2024-05-17 17:01:44 +0100

    Remove metadata from modal

--- src/app/@modal/(home)/nominate/[walletId]/page.tsx
+++ src/app/@modal/(home)/nominate/[walletId]/page.tsx
@@ -1,34 +1,3 @@
-import { Metadata } from "next";
-import { fetchMetadata } from "frames.js/next";
 import { default as NominateBuilder } from "@/app/@modal/_modals/nominate-builder/page";
-import { FRAMES_BASE_PATH, appURL } from "@/shared/frames/utils";
-
-const description = [
-  "BUILD is a meme and a social game designed to reward builders via",
-  "onchain nominations.",
-].join(" ");
-
-export async function generateMetadata({
-  params: { walletId },
-}: {
-  params: { walletId: string };
-}): Promise<Metadata> {
-  return {
-    title: "BUILD",
-    description: description,
-    openGraph: {
-      title: "BUILD",
-      description: description,
-      type: "website",
-      url: "https://build.top",
-      images: ["https://build.top/images/BUILD-thumbnail.jpg"],
-    },
-    other: {
-      ...(await fetchMetadata(
-        new URL(FRAMES_BASE_PATH + `/nominate/${walletId}`, appURL()),
-      )),
-    },
-  };
-}
 
 export default NominateBuilder;
