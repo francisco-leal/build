4e2e759903bd57929ddfba752ef2675b2a23b1b5
PupoSDC <pupo163@hotmail.com>
2024-05-20 23:23:39 +0100

    fix: avoid crash when cookie is corrupted

--- src/services/authentication/cookie-session.ts
+++ src/services/authentication/cookie-session.ts
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { unsealData } from "iron-session";
+import { rollbarError } from "../rollbar";
 
 const sessionPassword = process.env.SESSION_PASSWORD as string;
 if (!sessionPassword) throw new Error("SESSION_PASSWORD is not set");
@@ -16,13 +17,18 @@ export type SessionUser = {
 };
 
 export async function getSession(): Promise<SessionUser | null> {
-  const encryptedSession = cookies().get("auth_session")?.value;
+  try {
+    const encryptedSession = cookies().get("auth_session")?.value;
 
-  const session = encryptedSession
-    ? ((await unsealData(encryptedSession, {
-        password: sessionPassword,
-      })) as string)
-    : null;
-
-  return session ? (JSON.parse(session) as SessionUser) : null;
+    const session = encryptedSession
+      ? ((await unsealData(encryptedSession, {
+          password: sessionPassword,
+        })) as string)
+      : null;
+  
+    return session ? (JSON.parse(session) as SessionUser) : null;
+  } catch (e) {
+    rollbarError("Error retrieving user session", e as Error);
+    return null;
+  }
 }
