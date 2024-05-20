d8f2fa1dede3210ec2e3b96ad125730188dd3143
lim0n3 <simonestaffa96@gmail.com>
2024-05-20 15:46:23 +0200

    fix conflicts

--- src/app/api/action/route.tsx
+++ src/app/api/action/route.tsx
@@ -1,9 +1,6 @@
 import { NextRequest } from "next/server";
 import { ActionMetadata } from "frames.js";
-import {
-  getFarcasterUser,
-  getFarcasterUserById,
-} from "@/app/_api/external/farcaster";
+import { getFarcasterUserByFid } from "@/app/_api/external/farcaster";
 import { frames } from "../../frames/frames";
 
 export const GET = async (req: NextRequest) => {
@@ -22,7 +19,7 @@ export const GET = async (req: NextRequest) => {
 
 export const POST = frames(async (ctx) => {
   const castAuthorFid = ctx.message?.castId?.fid;
-  const castAuthor = await getFarcasterUserById(castAuthorFid!);
+  const castAuthor = await getFarcasterUserByFid(castAuthorFid!);
   if (!castAuthor) {
     return Response.json({
       type: "frame",
