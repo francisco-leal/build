a05aa8842321bae84404220cae697685c95059b8
PupoSDC <pupo163@hotmail.com>
2024-05-22 11:20:59 +0100

    feat: re-add leaderboard (#209)
    * feat: re add leaderboard
    
    * feat: remove useless cache expiration

---
+++ src/app/(pages)/leaderboard/page.tsx
@@ -0,0 +1,48 @@
+import { Stack, Typography } from "@mui/joy";
+import { DateTime } from "luxon";
+import { getCurrentUser } from "@/app/_api/data/users";
+import { getTableLeaderboardValues } from "@/app/_api/functions/get-table-leaderboard-values";
+import { HowToPlay } from "@/app/_components/how-to-play";
+import { PlaceholderUserNotConnected } from "@/app/_components/placeholder-user-not-connected";
+import { TableLeaderboard } from "@/app/_components/table-leaderboard";
+import { HeroSection } from "@/shared/components/hero-section";
+import { HeroSectionWithOverflow } from "@/shared/components/hero-section-with-overflow";
+
+export default async function AirdropPage() {
+  const user = await getCurrentUser();
+  if (!user) return <PlaceholderUserNotConnected />;
+
+  const now = DateTime.utc().startOf("hour");
+  const shortFormat = "LLL dd, hh:mm a 'UTC'";
+  const lastUpdate = now.toFormat(shortFormat);
+  const nextUpdate = now.plus({ hour: 1 }).toFormat(shortFormat);
+  const tableLeaderboardValues = await getTableLeaderboardValues();
+
+  return (
+    <Stack component="main">
+      <HeroSectionWithOverflow sx={{ mt: 0 }}>
+        <Typography
+          level="h2"
+          className="no-overflow"
+          textColor={"common.white"}
+        >
+          Leaderboard
+        </Typography>
+        <Stack className="overflow">
+          <TableLeaderboard values={tableLeaderboardValues} />
+        </Stack>
+        <Typography
+          className="no-overflow"
+          level="body-sm"
+          sx={{ color: "common.white" }}
+        >
+          Last update on {lastUpdate}. Next update on {nextUpdate}
+        </Typography>
+      </HeroSectionWithOverflow>
+
+      <HeroSection>
+        <HowToPlay />
+      </HeroSection>
+    </Stack>
+  );
+}
