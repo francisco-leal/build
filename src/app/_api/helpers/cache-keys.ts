485c3108451f2ab7d3422970d4c0ca91b0c48b9b
Francisco Leal <francisco@talentprotocol.com>
2024-05-28 14:58:21 +0100

    feat: Add top 5 nominations received (#238)
    * feat: Add top 5 nominations received
    
    * chore: change default cache time for nominations received

--- src/app/_api/helpers/cache-keys.ts
+++ src/app/_api/helpers/cache-keys.ts
@@ -13,7 +13,8 @@ export type CacheKey =
   | "nominations_count"
   | `stats_${string}`
   | `api_key_${string}`
-  | `api_user_${string}`;
+  | `api_user_${string}`
+  | `api_nominations_received_${string}`;
 
 export const CACHE_1_MINUTE = 60;
 export const CACHE_5_MINUTES = 60 * 5;
