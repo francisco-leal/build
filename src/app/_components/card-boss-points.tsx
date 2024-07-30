f1232b3b9bce8254b22e48ba0d0aae419016a79b
Francisco Leal <francisco@talentprotocol.com>
2024-07-30 15:54:05 +0100

    feat: update calculations of leaderboard and builder budget

--- src/app/_components/card-boss-points.tsx
+++ src/app/_components/card-boss-points.tsx
@@ -1,18 +1,23 @@
 import { FunctionComponent } from "react";
 import { Button, Stack, Typography, Link } from "@mui/joy";
+import { DateTime } from "luxon";
 import { BlockyCard } from "@/shared/components/blocky-card";
 import { Dice } from "@/shared/icons/dice";
 import { formatLargeNumber } from "@/shared/utils/format-number";
+import { getCurrentWeek } from "../_api/data/nominations";
 
 export type CardBossPointsProps = {
   points?: number;
   loading?: boolean;
 };
 
-export const CardBossPoints: FunctionComponent<CardBossPointsProps> = ({
+export const CardBossPoints: FunctionComponent<CardBossPointsProps> = async ({
   points = 0,
   loading,
 }) => {
+  const { endOfWeek } = await getCurrentWeek();
+  const shortFormat = "LLL dd, hh:mm a 'UTC'";
+  const nextUpdate = DateTime.fromISO(endOfWeek).toFormat(shortFormat);
   return (
     <BlockyCard>
       <Typography level="body-lg" textColor="primary.500">
@@ -30,7 +35,7 @@ export const CardBossPoints: FunctionComponent<CardBossPointsProps> = ({
 
       <Typography textColor="neutral.500">
         Total points earned from nominations received in Round 2, updated every
-        Tuesday. Next update on Jul 23.
+        Tuesday. Next update on {nextUpdate}.
       </Typography>
 
       <Button
