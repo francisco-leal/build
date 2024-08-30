4da7bce8016ac3ea3c88882f3fa88d0706c2bafe
Francisco Leal <francisco@talentprotocol.com>
2024-08-30 15:33:02 +0100

    change raffle base cost

--- src/app/_components/claim-section.tsx
+++ src/app/_components/claim-section.tsx
@@ -482,7 +482,7 @@ export const ClaimSection = ({ details, user }: Props) => {
                   <BlueCheck sx={{ "&&": { width: 24, height: 24 } }} />
                   <Typography level="body-sm" textAlign={"start"}>
                     Receive raffle tickets proportional to the burned amount to
-                    win the 5 ETH prize. Each 100k $BUILD burned is 1 ticket.
+                    win the 5 ETH prize. Each 10k $BUILD burned is 1 ticket.
                   </Typography>
                 </Stack>
                 <Stack
@@ -558,7 +558,7 @@ export const ClaimSection = ({ details, user }: Props) => {
                   <BlueCheck sx={{ "&&": { width: 24, height: 24 } }} />
                   <Typography level="body-sm" textAlign={"start"}>
                     Receive raffle tickets proportional to the burned amount to
-                    win the 5 ETH prize. Each 100k $BUILD burned is 1 ticket.
+                    win the 5 ETH prize. Each 10k $BUILD burned is 1 ticket.
                   </Typography>
                 </Stack>
                 <Stack
