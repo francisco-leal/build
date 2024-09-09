1dd0882e92efeced898029c67735e5595a613638
Francisco Leal <francisco@talentprotocol.com>
2024-09-09 14:07:32 +0100

    Add openframes middleware

--- src/app/frames/frames.ts
+++ src/app/frames/frames.ts
@@ -1,5 +1,7 @@
+import { openframes } from "frames.js/middleware";
 import { createFrames } from "frames.js/next";
 
 export const frames = createFrames({
   basePath: "/frames",
+  middleware: [openframes()],
 });
