a4376873c263a79665b6c10cd26cedaa207e7f0d
Francisco Leal <francisco@talentprotocol.com>
2024-09-09 14:42:14 +0100

    add setup for openframes

--- src/app/frames/frames.ts
+++ src/app/frames/frames.ts
@@ -1,5 +1,20 @@
+import { openframes } from "frames.js/middleware";
 import { createFrames } from "frames.js/next";
 
 export const frames = createFrames({
   basePath: "/frames",
+  middleware: [
+    openframes({
+      clientProtocol: {
+        id: "anonymous",
+        version: "2024-09-09",
+      },
+      handler: {
+        isValidPayload: (body: JSON) => true,
+        getFrameMessage: async (body: JSON) => {
+          return { ...body };
+        },
+      },
+    }),
+  ],
 });
